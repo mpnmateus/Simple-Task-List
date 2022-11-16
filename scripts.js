3d43dd179a5481728af61d6f9dc691ed00ef427b
Mateus Pettenuzzo <mpnmateus@gmail.com>
2022-11-16 19:34:28 -0300

    criando div add tarefa

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 const inputElement = document.querySelector(".new-task-input"); 
 const addTaskButton = document.querySelector(".new-task-button");
 
+const tasksContainer = document.querySelector(".tasks-container");
+
 const validateInput = () => inputElement.value.trim().lenght > 0;
     /*  
-    if (return inputElement.ariaValueMax.trim().lenght > 0) {
+    if (return inputElement.Value.trim().lenght > 0) {
         return true;
     } else {
         return false;
@@ -21,6 +23,31 @@ const handleAddTask = () => {
         return inputElement.classList.add("error");
     }
 
+    //Abaixo, criação da div no body do html em JS:
+    const taskItemContainer = document.createElement('div');
+    taskItemContainer.classList.add("task-item");
+
+    const taskContent = document.createElement("p");
+    taskContent.innerText = inputElement.value;
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("fa-trash-alt");
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+};
+
+const handleInputChange = () => {
+    const inputIsValid = validateInput ();
+
+    if (inputIsValid) {
+        return inputElement.classList.remove("error");
+    }
 };
 
 addTaskButton.addEventListener("click", () => handleAddTask());
+
+inputElement.addEventListener("change", () => handleInputChange());
