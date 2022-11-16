1d22f0fd2490eeb22512b7c818121c87045452d2
Mateus Pettenuzzo <mpnmateus@gmail.com>
2022-11-16 20:08:52 -0300

    add completar/deletar tarefa

--- scripts.js
+++ scripts.js
@@ -24,20 +24,46 @@ const handleAddTask = () => {
     }
 
     //Abaixo, criação da div no body do html em JS:
-    const taskItemContainer = document.createElement('div');
+    const taskItemContainer = document.createElement("div");
     taskItemContainer.classList.add("task-item");
 
     const taskContent = document.createElement("p");
     taskContent.innerText = inputElement.value;
 
+    taskContent.addEventListener("click", () => handleClick(taskContent));
+
     const deleteItem = document.createElement("i");
     deleteItem.classList.add("far");
     deleteItem.classList.add("fa-trash-alt");
+
+    deleteItem.addEventListener("click", () => handleDeleteCLick(taskItemContainer, taskContent));
 
     taskItemContainer.appendChild(taskContent);
     taskItemContainer.appendChild(deleteItem);
 
     tasksContainer.appendChild(taskItemContainer);
+
+    inputElement.value = "";
+};
+
+const handleCLick = (taskContent) => {
+    const tasks = tasksContainer.childNodes;
+    
+    for (const task of tasks) {
+        if (task.firstChild.isSameNode(taskContent)){
+            task.firstChild.classList.toggle("Completed");
+        }
+    }
+};
+
+const handleDeleteCLick = (taskItemContainer, taskContent) => {
+    const tasks = tasksContainer.childNodes;
+
+    for (const task of tasks) {
+        if (task.firstChild.isSameNode(taskContent)) {
+            taskItemContainer.remove();
+        }
+    }
 };
 
 const handleInputChange = () => {
