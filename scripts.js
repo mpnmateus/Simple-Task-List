e4a1b5a19ac43d3d691aa882b94148e270ec7c9e
Mateus Pettenuzzo <mpnmateus@gmail.com>
2022-11-16 21:56:05 -0300

    JS com salvamento armazenamento em local storage. bug em validação e add div

--- scripts.js
+++ scripts.js
@@ -44,26 +44,36 @@ const handleAddTask = () => {
     tasksContainer.appendChild(taskItemContainer);
 
     inputElement.value = "";
+
+    updateLocalStorage ()
 };
 
 const handleCLick = (taskContent) => {
     const tasks = tasksContainer.childNodes;
     
     for (const task of tasks) {
-        if (task.firstChild.isSameNode(taskContent)){
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
+        
+        if (currentTaskIsBeingClicked) {
             task.firstChild.classList.toggle("Completed");
         }
     }
+
+    updateLocalStorage ()
 };
 
 const handleDeleteCLick = (taskItemContainer, taskContent) => {
     const tasks = tasksContainer.childNodes;
 
     for (const task of tasks) {
-        if (task.firstChild.isSameNode(taskContent)) {
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
+        
+        if (currentTaskIsBeingClicked) {
             taskItemContainer.remove();
         }
     }
+
+    updateLocalStorage ()
 };
 
 const handleInputChange = () => {
@@ -73,6 +83,53 @@ const handleInputChange = () => {
         return inputElement.classList.remove("error");
     }
 };
+
+const updateLocalStorage = () => {
+    const tasks = tasksContainer.childNodes;
+
+    const localStorageTasks = [...tasks ].map((task) => {
+        const content = task.firstChild;
+        const isCompleted = content.classList.contains("completed");
+        return {description: content.innerText, isCompleted: isCompleted}
+    });
+
+    //console.log({localStorageTasks});
+    localStorage.setItem("tasks", JSON.stringify(localStorageTasks));
+};
+
+const refreshTaskUsingLocalStorage = () => {
+    const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+    //console.log({tasksFromLocalStorage});
+
+    if (!tasksFromLocalStorage) return;
+
+    for (const task of tasksFromLocalStorage) {
+    const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+
+    const taskContent = document.createElement("p");
+    taskContent.innerText = task.description;
+    
+    if (task.isCompleted) {
+        taskContent.classList.add("completed");
+    }
+
+    taskContent.addEventListener("click", () => handleClick(taskContent));
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("fa-trash-alt");
+
+    deleteItem.addEventListener("click", () => handleDeleteCLick(taskItemContainer, taskContent));
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+    }
+};
+
+refreshTaskUsingLocalStorage = ();
 
 addTaskButton.addEventListener("click", () => handleAddTask());
 
